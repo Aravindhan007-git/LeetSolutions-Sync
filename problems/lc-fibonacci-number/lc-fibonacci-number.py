9aa997aa3f79fbbaf787263b9eb3151a999c27b5
Aravindhan007-git <Aravindhan007-git@users.noreply.github.com>
2026-06-22 09:57:37 +0000

    [Dynamic Programming] Fibonacci Number solved

---
+++ problems/lc-fibonacci-number/lc-fibonacci-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        def f(n):
+            if n<=1:
+                return n
+            else:
+                return f(n-1)+f(n-2)
+        return f(n)
+            
